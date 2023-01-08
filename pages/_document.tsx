3bff57f83a39203375a61c69860e5038e40ff6c1
NeerajChinna123 <baipureddyneeraj@gmail.com>
2023-01-09 03:13:58 +0530

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link href="/static/2.79e41cda.chunk.css" rel="stylesheet" />
+        <link href="/static/main.e92e68f5.chunk.css" rel="stylesheet" />
+        <link href="/static/landingPage.css" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
