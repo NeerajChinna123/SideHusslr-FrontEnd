3c6c060d05404b2e3ecb949eff8a9d40cdd548f8
NeerajChinna123 <baipureddyneeraj@gmail.com>
2023-01-09 03:26:44 +0530

    moved static css code to global css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link href="/static/2.79e41cda.chunk.css" rel="stylesheet" />
+      {/* <link href="/static/2.79e41cda.chunk.css" rel="stylesheet" />
         <link href="/static/main.e92e68f5.chunk.css" rel="stylesheet" />
-        <link href="/static/landingPage.css" rel="stylesheet" />
+        <link href="/static/landingPage.css" rel="stylesheet" /> */}
       </Head>
       <body>
         <Main />
