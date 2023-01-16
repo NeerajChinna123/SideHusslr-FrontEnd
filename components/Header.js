30a1a309cf4d3bb0bd4097d050a77d80d11c3e77
NeerajChinna123 <baipureddyneeraj@gmail.com>
2023-01-16 12:34:53 +0530

    Changes in header component

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ import {
   UserCircleIcon,
   ArrowRightCircleIcon,
 } from "@heroicons/react/24/outline";
-import Link from "next/link";
+
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRef } from "react";
 import { useRouter } from "next/router";
@@ -71,85 +71,89 @@ function Header({ page }) {
           </div>
         </div>
         <div className="flex space-x-3 md:space-x-5 text-gray-100">
-          {status && (status != "authenticated" ? (
-            <motion.div
-            {...FADE_IN_ANIMATION_SETTINGS}
-              whileTap={{ scale: 0.96 }}
-              className="flex items-center space-x-2 transition-all duration-200 ease-in-out hover:text-[#f9004d] cursor-pointer p-2"
-            >
-              <div>
-                <p className="hidden md:block text-semibold tracking-wide text-center">
-                  Contact Us
-                </p>
-              </div>
-              <PhoneIcon className="md:h-5 md:w-5 h-6 w-6 animate-pulse text-semibold" />
-            </motion.div>
-          ) : page != "Banner" ? (
-            <motion.div
-            {...FADE_IN_ANIMATION_SETTINGS}
-              // whileTap={{ scale: 0.96 }}
-              className="flex items-center space-x-2 transition-all duration-200 ease-in-out hover:text-[#f9004d]  p-2"
-            >
-              <UserCircleIcon className="md:h-5 md:w-5 h-7 w-7 animate-pulse text-semibold" />
-              <div>
-                <p className="hidden md:block text-semibold tracking-wide text-center">
-                  {session.data[0].first_name}
-                </p>
-              </div>
-            </motion.div>
-          ) : (
-            <motion.div
-            {...FADE_IN_ANIMATION_SETTINGS}
-              whileTap={{ scale: 0.96 }}
-              onClick={() => {
-                router.push("/redirect");
-              }}
-              className="flex items-center  tracking-wide space-x-2 bg-black border rounded-lg px-2 cursor-pointer justify-center shadow-md shadow-red-800 border-[#f9004d] transition-all duration-300 ease-in-out border-solid  hover:bg-[#f9004d] hover:text-white "
-            >
-              <div className="py-2">
-                <p className=" text-center tracking-wider text-white font-semibold">
-                  Get Started
-                </p>
-              </div>
-              <ArrowRightCircleIcon className="h-6 w-6 animate-pulse text-semibold" />
-            </motion.div>
-          ))}
-
-          {(status && status != "authenticated" ? (
-            <motion.div
-              {...FADE_IN_ANIMATION_SETTINGS}
-              whileTap={{ scale: 0.96 }}
-              onClick={() => {
-                signInFun();
-              }}
-              className="flex items-center tracking-wide space-x-2 bg-black border rounded-lg px-2 py-2 md:py-0 cursor-pointer justify-center shadow-md shadow-red-800 border-[#f9004d] transition-all duration-300 ease-in-out border-solid  hover:bg-[#f9004d] hover:text-white "
-            >
-              <div>
-                <p className=" text-center tracking-wider text-white font-semibold">
-                  Sign In
-                </p>
-              </div>
-              <ArrowRightOnRectangleIcon className="h-6 w-6 animate-pulse text-semibold" />
-            </motion.div>
-          ) : (
-            page != "Banner" && (
+          {status &&
+            (status == "unauthenticated" ? (
               <motion.div
-              {...FADE_IN_ANIMATION_SETTINGS}
+                {...FADE_IN_ANIMATION_SETTINGS}
                 whileTap={{ scale: 0.96 }}
-                onClick={() => {
-                  signOutFun();
-                }}
-                className="flex items-center tracking-wide space-x-2 bg-black border rounded-lg px-2 py-2 md:py-0 cursor-pointer justify-center shadow-md shadow-red-800 border-[#f9004d] transition-all duration-300 ease-in-out border-solid  hover:bg-[#f9004d] hover:text-white "
+                className="flex items-center space-x-2 transition-all duration-200 ease-in-out hover:text-[#f9004d] cursor-pointer p-2"
               >
-                <ArrowLeftOnRectangleIcon className="h-6 w-6 animate-pulse text-semibold" />
                 <div>
-                  <p className=" text-center tracking-wider text-white font-semibold">
-                    Sign Out
+                  <p className="hidden md:block text-semibold tracking-wide text-center">
+                    Contact Us
+                  </p>
+                </div>
+                <PhoneIcon className="md:h-5 md:w-5 h-6 w-6 animate-pulse text-semibold" />
+              </motion.div>
+            ) : page != "Banner" ? (
+              <motion.div
+                {...FADE_IN_ANIMATION_SETTINGS}
+                // whileTap={{ scale: 0.96 }}
+                className="flex items-center space-x-2 transition-all duration-200 ease-in-out hover:text-[#f9004d]  p-2"
+              >
+                <UserCircleIcon className="md:h-5 md:w-5 h-7 w-7 animate-pulse text-semibold" />
+                <div>
+                  <p className="hidden md:block text-semibold tracking-wide text-center">
+                    {session.data[0].first_name}
                   </p>
                 </div>
               </motion.div>
-            )
-          ))}
+            ) : (
+              status == "authenticated" && (
+                <motion.div
+                  {...FADE_IN_ANIMATION_SETTINGS}
+                  whileTap={{ scale: 0.96 }}
+                  onClick={() => {
+                    router.push("/redirect");
+                  }}
+                  className="flex items-center  tracking-wide space-x-2 bg-black border rounded-lg px-2 cursor-pointer justify-center shadow-md shadow-red-800 border-[#f9004d] transition-all duration-300 ease-in-out border-solid  hover:bg-[#f9004d] hover:text-white "
+                >
+                  <div className="py-2">
+                    <p className=" text-center tracking-wider text-white font-semibold">
+                      Get Started
+                    </p>
+                  </div>
+                  <ArrowRightCircleIcon className="h-6 w-6 animate-pulse text-semibold" />
+                </motion.div>
+              )
+            ))}
+
+          {status &&
+            (status == "unauthenticated" ? (
+              <motion.div
+                {...FADE_IN_ANIMATION_SETTINGS}
+                whileTap={{ scale: 0.96 }}
+                onClick={() => {
+                  signInFun();
+                }}
+                className="flex items-center tracking-wide space-x-2 bg-black border rounded-lg px-2 py-2 md:py-0 cursor-pointer justify-center shadow-md shadow-red-800 border-[#f9004d] transition-all duration-300 ease-in-out border-solid  hover:bg-[#f9004d] hover:text-white "
+              >
+                <div>
+                  <p className=" text-center tracking-wider text-white font-semibold">
+                    Sign In
+                  </p>
+                </div>
+                <ArrowRightOnRectangleIcon className="h-6 w-6 animate-pulse text-semibold" />
+              </motion.div>
+            ) : (
+              page != "Banner" && (
+                <motion.div
+                  {...FADE_IN_ANIMATION_SETTINGS}
+                  whileTap={{ scale: 0.96 }}
+                  onClick={() => {
+                    signOutFun();
+                  }}
+                  className="flex items-center tracking-wide space-x-2 bg-black border rounded-lg px-2 py-2 md:py-0 cursor-pointer justify-center shadow-md shadow-red-800 border-[#f9004d] transition-all duration-300 ease-in-out border-solid  hover:bg-[#f9004d] hover:text-white "
+                >
+                  <ArrowLeftOnRectangleIcon className="h-6 w-6 animate-pulse text-semibold" />
+                  <div>
+                    <p className=" text-center tracking-wider text-white font-semibold">
+                      Sign Out
+                    </p>
+                  </div>
+                </motion.div>
+              )
+            ))}
         </div>
       </div>
       {/* <div
