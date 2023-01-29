4fbca2fae894b74f61e0edf0642d625919020766
NeerajChinna123 <baipureddyneeraj@gmail.com>
2023-01-29 18:17:00 +0530

    Added responsiveness for course-details component

--- containers/CourseBanner.tsx
+++ containers/CourseBanner.tsx
@@ -1,17 +1,10 @@
 import Image from "next/image";
-import Balancer from "react-wrap-balancer";
 import { studentDataType, StudentAssignmentInstructorsType } from "../typings";
 import {
   DocumentTextIcon,
   ShieldCheckIcon,
   ExclamationCircleIcon,
 } from "@heroicons/react/24/outline";
-
-import {
-  CircularProgressbar,
-  CircularProgressbarWithChildren,
-  buildStyles,
-} from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import CircularProgress from "../components/CircularProgress";
 
@@ -37,11 +30,13 @@ function CourseBanner(props: StudentData) {
 
   console.log("p-dt : ", props.data);
 
-  const percentage = 20;
+  const completedPercent =
+    (assignmentCompleted?.length / assignmentsCount) * 100;
+
   return (
-    <div className="max-w-7xl mx-auto mt-20">
-      <div className="flex flex-row space-x-14">
-        <div className="relative h-[22rem] w-[20rem] ">
+    <div className="max-w-7xl mx-auto mt-10 lg:mt-20 md:px-4 lg:px-0">
+      <div className="flex flex-col md:flex-row ">
+        <div className="relative self-center md:self-start h-[22rem] w-[22rem] md:h-[28rem] lg:h-[25rem]">
           <Image
             alt=""
             className="rounded-md shadow-lg shadow-gray-400 "
@@ -54,19 +49,19 @@ function CourseBanner(props: StudentData) {
             <p>Semester - 1</p>
           </div>
         </div>
-        <div className="flex flex-col space-y-3 flex-1">
-          <div className="w-[100%]">
-            <Balancer className="text-black font-bold font-poppins tracking-wide leading-[6rem] text-[4rem]">
+        <div className="flex flex-col lg:h-[25rem] md:h-[28rem] items-center md:items-start space-y-4 pt-6 md:pt-0 md:ml-6  flex-1 lg:ml-16">
+          <div className="w-[93%] md:w-[100%] self-center md:self-start pl-2 md:pl-0">
+            <p className="text-black font-bold font-poppins tracking-wide leading-[3rem] lg:leading-[4.1rem] text-[3rem] lg:text-[3.4rem]">
               {props?.data?.Course?.name}
-            </Balancer>
+            </p>
           </div>
-          <div className="w-[95%] max-h-[8rem] h-[8rem] overflow-y-scroll scrollbar-thin scrollbar-thumb-red-500 scrollbar-track-gray-200 scrollbar-thumb-rounded-full scrollbar-track-rounded-full  scrollbar-thumb-h-[2rem]  scrollbar-w-[0.3rem] md:scrollbar-w-[0.2rem] ml-2">
-            <p className="tracking-wide text-[1rem] font-poppins text-stone-600 pr-[0.8rem]">
+          <div className="lg:w-[95%] w-[90%] max-h-[7rem] h-[7rem] md:h-[9rem] md:max-h-[9rem] lg:max-h-[13rem] lg:h-[13rem] overflow-y-scroll scrollbar-thin scrollbar-thumb-red-500 scrollbar-track-gray-200 scrollbar-thumb-rounded-full scrollbar-track-rounded-full  scrollbar-thumb-h-[2rem]  scrollbar-w-[0.3rem] md:scrollbar-w-[0.2rem] ml-2">
+            <p className="tracking-wide text-[0.9rem] lg:text-[1rem] font-poppins text-stone-600 pr-[0.8rem]">
               {props?.data?.Course?.description}
             </p>
           </div>
-          <div className="flex w-[50%] flex-row pt-2 justify-between">
-            <div className="flex flex-col space-y-4 border rounded-2xl justify-center items-center border-gray-200 px-3 py-2">
+          <div className="hidden lg:flex w-[90%] pt-6 flex-wrap md:w-[50%] self-start ml-5 flex-row lg:pt-2 lg:ml-1 lg:justify-between">
+            <div className="flex flex-col space-y-4 border rounded-2xl mr-3 md:mr-0 justify-center items-center border-gray-200 px-2 md:px-3 py-2">
               <DocumentTextIcon className="text-black font-bold h-6 w-6" />
               <div className="bg-gray-100 rounded-xl px-6 w-[100%] py-2">
                 <p className="font-semibold text-sm tracking-wide">
@@ -74,16 +69,16 @@ function CourseBanner(props: StudentData) {
                 </p>
               </div>
             </div>
-            <div className="flex flex-col space-y-4 border rounded-2xl justify-center items-center border-gray-200 px-2">
-              <ShieldCheckIcon className="text-black font-bold h-6 w-6" />
+            <div className="flex flex-col space-y-4 border rounded-2xl justify-center items-center border-gray-200 px-2 md:px-3 py-2">
+              <ShieldCheckIcon className="text-green-600 font-bold h-6 w-6" />
               <div className="bg-gray-100 rounded-xl px-6 w-[100%] py-2">
                 <p className="font-semibold text-sm tracking-wide">
                   {assignmentCompleted?.length}
                 </p>
               </div>
             </div>
-            <div className="flex flex-col space-y-4 border rounded-2xl justify-center items-center border-gray-200 px-2">
-              <ExclamationCircleIcon className="text-black font-bold h-6 w-6" />
+            <div className="flex flex-col mt-3 md:mt-0 space-y-4 border rounded-2xl mr-3 md:mr-0 justify-center items-center border-gray-200 px-2 md:px-3 py-2">
+              <ExclamationCircleIcon className="text-red-600 font-bold h-6 w-6" />
               <div className="bg-gray-100 rounded-xl px-6 w-[100%] py-2">
                 <p className="font-semibold text-sm tracking-wide">
                   {assignmentPending?.length}
@@ -91,16 +86,55 @@ function CourseBanner(props: StudentData) {
               </div>
             </div>
           </div>
+          <div className="flex flex-row px-5 md:px-2 lg:hidden">
+            <div className="flex  md:pt-3 pt-6 flex-wrap w-[54%]  flex-row">
+              <div className="flex flex-col space-y-4 border rounded-2xl mr-3 lg:mr-0 justify-center items-center border-gray-200 px-2 lg:px-3 lg-2">
+                <DocumentTextIcon className="text-black font-bold h-6 w-6" />
+                <div className="bg-gray-100 rounded-xl px-6 w-[100%] py-2">
+                  <p className="font-semibold text-sm tracking-wide">
+                    {assignmentsCount}
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col space-y-4 border rounded-2xl justify-center items-center border-gray-200 px-2 lg:px-3 py-2">
+                <ShieldCheckIcon className="text-green-600 font-bold h-6 w-6" />
+                <div className="bg-gray-100 rounded-xl px-6 w-[100%] py-2">
+                  <p className="font-semibold text-sm tracking-wide">
+                    {assignmentCompleted?.length}
+                  </p>
+                </div>
+              </div>
+              <div className="flex flex-col mt-3 lg:mt-0 space-y-4 border rounded-2xl mr-3 lg:mr-0 justify-center items-center border-gray-200 px-2 lg:px-3 py-2">
+                <ExclamationCircleIcon className="text-red-600 font-bold h-6 w-6" />
+                <div className="bg-gray-100 rounded-xl px-6 w-[100%] py-2">
+                  <p className="font-semibold text-sm tracking-wide">
+                    {assignmentPending?.length}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className=" self-center  lg:hidden mr-7 md:mr-8">
+              <div className="h-[9rem]  border-l border-gray-200"></div>
+            </div>
+            <div className="lg:hidden self-center items-center">
+              <CircularProgress
+                size={138}
+                strokeWidth={12}
+                percentage={completedPercent}
+                color="green"
+              />
+            </div>
+          </div>
         </div>
-        <div className="flex items-center w-[1%]">
+        <div className=" items-center w-[1.4%] hidden lg:flex mr-8">
           <div className="h-[14rem]  border-l border-gray-200 shadow shadow-gray-200"></div>
         </div>
-        <div className="flex items-center">
+        <div className="hidden lg:flex items-center">
           <CircularProgress
-            size={210}
+            size={220}
             strokeWidth={18}
-            percentage={30}
-            color="red"
+            percentage={20}
+            color="#16A34A"
           />
         </div>
       </div>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,67 +2,75 @@ import NextAuth from "next-auth";
 import axios from "axios";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export default NextAuth({
-  providers: [
-    CredentialsProvider({
-      async authorize(credentials, req) {
-        const payload = {
-          username: credentials.username,
-          password: credentials.password,
-        };
-        const customConfig = {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        };
 
-        try {
-          const res = await axios.post(
-            `${process.env.SIDEHUSSLR_API}/login`,
-            payload,
-            customConfig
-          );
 
-          const user = await res.data;
-          if (res?.data?.success == false) {
-            throw new Error(user.error);
+const nextAuthOptions = (req, res) => {
+  return {
+    providers: [
+      CredentialsProvider({
+        async authorize(credentials, req,res) {
+          const payload = {
+            username: credentials.username,
+            password: credentials.password,
+          };
+          const customConfig = {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          };
+  
+          try {
+            const res = await axios.post(
+              `${process.env.SIDEHUSSLR_API}/login`,
+              payload,
+              customConfig
+            );
+  
+            const user = await res.data;
+            if (res?.data?.success == false) {
+              throw new Error(user.error);
+            }
+            // If no error and we have user data, return it
+            if (res?.data?.success == true) {
+              return user;
+            }
+            // Work with the response...
+          } catch (err) {
+            // Handle error
+            console.log(err);
+            return null;
           }
-          // If no error and we have user data, return it
-          if (res?.data?.success == true) {
-            return user;
-          }
-          // Work with the response...
-        } catch (err) {
-          // Handle error
-          console.log(err);
+  
+          // Return null if user data could not be retrieved
           return null;
+        },
+      }),
+    ],
+    // jwt: { encryption: false },
+    secret: process.env.JWT_SECRET,
+    pages: {
+      signIn: "/auth/signIn",
+    },
+    callbacks: {
+      async jwt({ token, user, account }) {
+        if (account && user) {
+          return {
+            ...token,
+            ...user,
+            accessToken: user.token,
+          };
         }
-
-        // Return null if user data could not be retrieved
-        return null;
+        return token;
       },
-    }),
-  ],
-  // jwt: { encryption: false },
-  secret: process.env.JWT_SECRET,
-  pages: {
-    signIn: "/auth/signIn",
-  },
-  callbacks: {
-    async jwt({ token, user, account }) {
-      if (account && user) {
-        return {
-          ...token,
-          ...user,
-          accessToken: user.token,
-        };
-      }
-      return token;
+      async session({ session, user, token }) {
+        return { ...token };
+      },
     },
-    async session({ session, user, token }) {
-      return { ...token };
-    },
-  },
-  // Enable debug messages in the console if you are having problems
-  debug: process.env.NODE_ENV === "development",
-});
+    // Enable debug messages in the console if you are having problems
+    debug: process.env.NODE_ENV === "development",
+  }
+}
+
+export default (req, res) => {
+  return NextAuth(req, res, nextAuthOptions(req, res))
+}
